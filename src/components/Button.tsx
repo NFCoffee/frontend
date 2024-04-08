dfead1c7c84aaa88291ed6360eaadd560769c86e
jean <20massalia@kakao.com>
2024-04-08 19:49:44 +0900

    feat: PrivateKeyScreen created

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,35 +1,36 @@
-import React from "react";
-import { StyleSheet, Text, TouchableOpacity } from "react-native";
+// Button.tsx
+import React from 'react';
+import { StyleSheet, Text, TouchableOpacity, ViewStyle } from 'react-native';
 import { COLOR } from '../utils/color';
 
 interface ButtonProps {
-    buttonText: string;
-    style?: object; // Add style prop to ButtonProps
+  buttonText: string;
+  style?: ViewStyle;
+  onPress?: () => void; // Add onPress prop
 }
 
-const Button: React.FC<ButtonProps> = ({ buttonText, style }) => {
-    return (
-        <TouchableOpacity style={[styles.touchop, style]}>
-            <Text style={styles.text}>{buttonText}</Text>
-        </TouchableOpacity>
-    );
+const Button: React.FC<ButtonProps> = ({ buttonText, style, onPress }) => {
+  return (
+    <TouchableOpacity style={[styles.touchop, style]} onPress={onPress}>
+      <Text style={styles.text}>{buttonText}</Text>
+    </TouchableOpacity>
+  );
 };
 
 const styles = StyleSheet.create({
-    touchop: {
-        backgroundColor: COLOR.blue,
-        width: '100%',
-        height: 50,
-        alignItems: 'center',
-        justifyContent: 'center',
-        borderRadius: 8,
-        margin: 10,
-    },
-    text: {
-        fontFamily: 'SeoulNamsanB',
-        fontSize: 22,
-        color: 'white',
-    },
+  touchop: {
+    backgroundColor: COLOR.blue,
+    width: '100%',
+    height: '6.5%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 8,
+  },
+  text: {
+    fontFamily: 'SeoulNamsanB',
+    fontSize: 22,
+    color: 'white',
+  },
 });
 
 export default Button;
