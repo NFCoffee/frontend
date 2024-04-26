7318f1bcd2543cef4b8e06bc18120f6f156815af
jean <20massalia@kakao.com>
2024-04-26 23:21:42 +0900

    feat: Added PaymentScreen

---
+++ src/screens/PaymentScreen.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { COLOR } from "../utils/color";
+import { SafeAreaView, ScrollView, StyleSheet, Text, View } from "react-native";
+import Button from "../components/Button";
+
+export default function PaymentScreen() {
+    
+}
