e7f87a06b734497bae1ca91b0097102431fdcc16
조성열 <cho-seongyur@joseong-yeol-ui-MacBookPro.local>
2024-05-04 19:44:17 +0900

    20240504

--- App.tsx
+++ App.tsx
@@ -5,6 +5,11 @@ import MainScreen from "./src/screens/MainScreen";
 import TransactionScreen from "./src/screens/TransactionScreen";
 import SettingScreen from "./src/screens/SettingScreen";
 import OrderScreen from "./src/screens/OrderScreen";
+import PaymentScreen from "./src/screens/PaymentScreen";
+import LoginScreen from "./src/screens/LoginScreen";
+import MembershipScreen from "./src/screens/MembershipScreen";
+import CertificationNumScreen from "./src/screens/CertificationNumScreen";
+import LostKeyScreen from "./src/screens/LostKeyScreen";
 
 export default function App() {
   return (
@@ -14,7 +19,9 @@ export default function App() {
       {/* <MainScreen/> */}
       {/* <TransactionScreen/> */}
       {/* <SettingScreen/> */}
-      <OrderScreen/>
-    </> 
+      <MembershipScreen/>
+      {/* <CertificationNumScreen/> */}
+      {/* <LostKeyScreen/> */}
+    </>
   )
 }
