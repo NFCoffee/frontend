aa768611bceffef40358e640e98402589a99f93e
Choseongyul <tjdduf0504@naver.com>
2024-06-07 19:28:38 +0900

    fix LoginScreen replace pinNumScreen

--- App.tsx
+++ App.tsx
@@ -1,3 +1,47 @@
+// import React from "react";
+// import LoginScreen from "./src/screens/LoginScreen";
+// import TabNavigation from './src/navigation/TapNavigation';
+// import { NavigationContainer } from '@react-navigation/native';
+// import { createNativeStackNavigator } from '@react-navigation/native-stack';
+// import MembershipScreen from "./src/screens/MembershipScreen";
+// import CertificationNumScreen from "./src/screens/CertificationNumScreen";
+// import PrivateKeyScreen from "./src/screens/PrivateKeyScreen";
+// import NFTScreen from "./src/screens/NFTScreen";
+// import LostKeyScreen from "./src/screens/LostKeyScreen";
+// import TransactionScreen from "./src/screens/TransactionScreen";
+// import OrderScreen from "./src/screens/OrderScreen";
+// import PaymentScreen from "./src/screens/PaymentScreen";
+// import PaymentSuccessScreen from "./src/screens/PaymentSuccess";
+// import { PrivateKeyProvider } from './src/context/PrivateKeyContext';
+// import PinNumScreen from './src/screens/PinNumScreen';
+
+// const Stack = createNativeStackNavigator();
+
+// export default function App() {
+//   return ( 
+//     <>
+//     <PrivateKeyProvider>
+//       <NavigationContainer>
+//         <Stack.Navigator initialRouteName="Login" screenOptions={{ headerShown: false }}>
+//           <Stack.Screen name="Login" component={LoginScreen} />
+//           <Stack.Screen name="Certification" component={CertificationNumScreen} />
+//           <Stack.Screen name="Signup" component={MembershipScreen} />
+//           <Stack.Screen name="Privatekey" component={PrivateKeyScreen} />
+//           <Stack.Screen name="PinNum" component={PinNumScreen} />
+//           <Stack.Screen name="NFT" component={NFTScreen} />
+//           <Stack.Screen name="Lostkey" component={LostKeyScreen} />
+//           <Stack.Screen name="History" component={TransactionScreen} />
+//           <Stack.Screen name="Order" component={OrderScreen} />
+//           <Stack.Screen name="Payment" component={PaymentScreen} />
+//           <Stack.Screen name="PaymentSuccess" component={PaymentSuccessScreen} />
+//           <Stack.Screen name="Tab" component={TabNavigation} />
+//         </Stack.Navigator>
+//       </NavigationContainer>
+//     </PrivateKeyProvider>=
+//     </>
+//   )
+// }
+
 import React from "react";
 import LoginScreen from "./src/screens/LoginScreen";
 import TabNavigation from './src/navigation/TapNavigation';
@@ -20,13 +64,14 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return ( 
     <>
-    {/* <PrivateKeyProvider>
+    <PrivateKeyProvider>
       <NavigationContainer>
         <Stack.Navigator initialRouteName="Login" screenOptions={{ headerShown: false }}>
           <Stack.Screen name="Login" component={LoginScreen} />
-          <Stack.Screen name="Signup" component={MembershipScreen} />
           <Stack.Screen name="Certification" component={CertificationNumScreen} />
+          <Stack.Screen name="Signup" component={MembershipScreen} />
           <Stack.Screen name="Privatekey" component={PrivateKeyScreen} />
+          <Stack.Screen name="PinNumScreen" component={PinNumScreen} />
           <Stack.Screen name="NFT" component={NFTScreen} />
           <Stack.Screen name="Lostkey" component={LostKeyScreen} />
           <Stack.Screen name="History" component={TransactionScreen} />
@@ -36,8 +81,7 @@ export default function App() {
           <Stack.Screen name="Tab" component={TabNavigation} />
         </Stack.Navigator>
       </NavigationContainer>
-    </PrivateKeyProvider> */}
-      <PinNumScreen/>
+    </PrivateKeyProvider>
     </>
   )
 }
